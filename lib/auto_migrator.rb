8eee807116ffd23888b9ffa3745dcbdc56543797
Ryan Sandridge <noreply@ryansandridge.com>
2023-05-10 18:42:12 -0400

    Bundler generated gem skeleton

---
+++ lib/auto_migrator.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "auto_migrator/version"
+
+module AutoMigrator
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/auto_migrator/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module AutoMigrator
+  VERSION = "0.1.0"
+end
